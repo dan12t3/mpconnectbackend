8d441b5255a0f60e694983459b3560d01bdf417d
dan12t3 <dan12t3@gmail.com>
2017-08-28 18:53:21 -0400

    writes to DB

--- src/components/Store/register.js
+++ src/components/Store/register.js
@@ -1,16 +1,52 @@
 const express = require('express');
 const router = express.Router();
 const console = require('console');
+const mysql = require('mysql');
+const config = require('../../dbConfig.js');
 
 router.use('/',(req,res,next) => {
   //connect to db
-  next();
+
+  req.conn = mysql.createConnection(config);
+  req.conn.connect((err) => {
+    if(err) {
+      console.log(err);
+      res.end(err);
+    }
+    else {
+      console.log('DB Connection Established');
+      next();
+    }
+  });
+
+
 });
 
 router.post('/newuser',(req,res) => {
+
+  const { username, password, firstname, lastname, email, phone } = req.body;
   //inputs new user to db
-  console.log(req.body);
-  res.end();
+
+  let query = "INSERT INTO ?? (??, ??, ??, ??, ??, ??) VALUES (?, ?, ?, ?, ?, ?)";
+  const insert = ['users','username','password','firstname','lastname','email','phone',username,password,firstname,lastname,email,phone];
+
+  query = mysql.format(query,insert);
+
+  req.conn.query(query, err=> {
+
+    if(err){
+      console.log(err);
+      req.conn.end();
+      res.end(err);
+    }else{
+      console.log("Query successful");
+      req.conn.end();
+      res.end()
+    }
+  });
+
+  //console.log(req.body);
+  //res.end();
 });
 
 router.get('/validateuser',(req,res) => {
--- src/dbConfig.js
+++ src/dbConfig.js
@@ -2,7 +2,7 @@ const config = {
   host: 'localhost',
   user: 'root',
   password: '',
-  database: 'mpconnect'
+  database: 'raven'
 }
 
 module.exports = config;
