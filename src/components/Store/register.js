ae7d4de5b00981b0a609bf70cda55f3124bd5e80
dan12t3 <dan12t3@gmail.com>
2017-08-27 21:21:08 -0400

    added new routes for registeration

--- src/components/Store/register.js
+++ src/components/Store/register.js
@@ -2,16 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/',(req,res,next) => {
-
+  //connect to db
   next();
 });
 
 router.post('/newuser',(req,res) => {
+  //inputs new user to db
   res.end();
 });
 
 router.get('/validateuser',(req,res) => {
-  
+  //checks if username and email are valid
   res.end();
 });
 
